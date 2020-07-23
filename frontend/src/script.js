71fe37424c1f9823f1b26ac93128541ddbf6448e
Patrick Moran <patrickmoran@Patricks-MacBook-Pro-2.local>
2020-07-22 21:29:59 -0700

    goog

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,1 +1,12 @@
-console.log('hello there');
+
+
+
+new Date($.now());
+
+
+
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+document.write(time);
+
+$(Date).addClass(a);
