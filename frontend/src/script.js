09f034ff3290fe5b62f6f1748cfc495ee28c6ac4
Patrick Moran <patrickmoran@Patricks-MacBook-Pro-2.local>
2020-07-25 23:17:32 -0700

    indented html because kristian is ocd

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,19 +1,10 @@
 
 
-
-// new Date($.now());
-
-
-
+// get hour of the day
 var dt = new Date();
 var time = dt.getHours();
 
-console.log(time);
-// document.write(time);
-
-// $(Date).addClass(a);
-
-
+// if night time, add "nightmode" class
 if (time >= 18 || time <= 6) {
     $('.paragraph').addClass('nightmode');
     $('.zoo').addClass('nightmode');
